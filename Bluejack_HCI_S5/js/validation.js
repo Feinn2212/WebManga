827f021bf22338c24e9605de0e73b99a1353011a
Feinn2212 <andrewirawan55@gmail.com>
2022-01-18 12:07:10 +0700

    some

--- Bluejack_HCI_S5/js/validation.js
+++ Bluejack_HCI_S5/js/validation.js
@@ -8,12 +8,33 @@ const errorElement = document.getElementById('error')
 
 form.addEventListener('submit', (e) => {
     let messages = [];
-    if(password.value.length <= 6){
-        messages.push('Password must be longer than 6 characters');
+    if(password.value.length <= 5){
+        alert('Password must be longer than 5 characters');
     }
 
     if(password.value.length >= 20){
-        messages.push('Password must be less than 20 characters');
+        alert('Password must be less than 20 characters');
+    }
+
+    if(password.value != passwordConfirm.value){
+        alert("Password does not match!");
+    }
+
+
+    function onlyNumberKey(evt) {
+          
+        // Only ASCII character in that range allowed
+        var ASCIICode = (evt.which) ? evt.which : evt.keyCode
+        if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+            return false;
+        return true;
+    }
+
+    if(tel.value.length <= 7){
+        alert("Phone number must be longer than 7 characters");
+    }
+    else{
+        
     }
 
     if(messages.length > 0){
