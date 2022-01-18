451efd71595c69f4036590a80ef8907d8edacd6a
shuum1n <verdiankrishnanda02@gmail.com>
2022-01-18 12:08:37 +0800

    fixed form

--- Bluejack_HCI_S5/js/validation.js
+++ Bluejack_HCI_S5/js/validation.js
@@ -1,23 +1,24 @@
 
-const email = document.getElementById('email')
-const password = document.getElementById('password')
-const passwordConfirm = document.getElementById('passwordConfirm')
-const tel = document.getElementById('tel')
-const form = document.getElementById('form')
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const passwordConfirm = document.getElementById('passwordConfirm');
+const tel = document.getElementById('tel');
+const form = document.getElementById('form');
+const errorElement = document.getElementById('error')
 
 form.addEventListener('submit', (e) => {
-    let messages =[]
-    if(password.value.lenght <= 6){
-        messages.push('Password must be longer than 6 characters')
+    let messages = [];
+    if(password.value.length <= 6){
+        messages.push('Password must be longer than 6 characters');
     }
 
-    if(password.value.lenght >= 20){
-        messages.push('Password must be less than 20 characters')
+    if(password.value.length >= 20){
+        messages.push('Password must be less than 20 characters');
     }
 
-    if(messages.lenght > 0){
-        e.preventDefault()
-        errorElement.innerText = messages.join(', ')
+    if(messages.length > 0){
+        e.preventDefault();
+        errorElement.innerText = messages.join(', ');
     }
 })
 
